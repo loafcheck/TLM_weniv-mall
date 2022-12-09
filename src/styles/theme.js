dcaa8cc662f3814e8e24610e1494dea9ff191f3a
Rachel S Kim / 김민정 <106638262+loafcheck@users.noreply.github.com>
2022-12-10 01:11:34 +0900

    Create theme.js

---
+++ src/styles/theme.js
@@ -0,0 +1,13 @@
+const commonTheme = {
+  pointColor: "#6327FE",
+  filterColor: "#0000004E", // 16진수 0 ~ 9 A B C D E F
+  lightBlack: "#333333",
+};
+const light = {
+  ...commonTheme,
+};
+const dark = {
+  ...commonTheme,
+};
+
+export { light, dark };
